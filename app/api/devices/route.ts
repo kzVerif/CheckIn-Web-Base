2ac50b56c7f6053e011f60987416d081df60c12c
KzVerif <kanghunproject@gmail.com>
2025-10-24 14:13:32 +0700

    api register login change schema

--- app/api/devices/route.ts
+++ app/api/devices/route.ts
@@ -1,32 +1,40 @@
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcrypt";
 const prisma = new PrismaClient();
 
-//get all device
-export async function GET(req: NextRequest , res: NextResponse) { 
-    try {
-        const data = await prisma.device.findMany();
-        return NextResponse.json({
-            message: "ดึงข้อมูล device ทั้งหมดสำเร็จ", 
-            data: data,
-        },
-    {
-        status: 200,
-    }
-);
-} catch (error) { 
-    console.log("Cannot get all  devices:" , error);
-    return NextResponse.json(
-        {
-            message: " ไม่สามารถดึงข้อมูล device ทั้งหมดได้ ",
-            reason: error,
-        },
-        {
-            status: 500,
-        }
-
-    );
+interface registerData {
+  device_id: string;
+  student_id: string;
+  name: string;
+  email: string;
+  password: string;
+  rePassword: string;
 }
 
-} 
-    
+//get all device
+export async function GET(req: NextRequest, res: NextResponse) {
+  try {
+    const data = await prisma.devices.findMany();
+    return NextResponse.json(
+      {
+        message: "ดึงข้อมูล device ทั้งหมดสำเร็จ",
+        data: data,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.log("Cannot get all  devices:", error);
+    return NextResponse.json(
+      {
+        message: " ไม่สามารถดึงข้อมูล device ทั้งหมดได้ ",
+        reason: error,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}

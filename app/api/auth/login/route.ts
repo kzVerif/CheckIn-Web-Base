287e0a710a00240a4cbcada0333de9345e201ea3
KzVerif <kanghunproject@gmail.com>
2025-10-25 09:38:16 +0700

    update middleware

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -56,3 +56,11 @@ export async function POST(req: NextRequest) {
     );
   }
 }
+
+// export async function OPTIONS() {
+//   const res = new NextResponse(null, { status: 204 });
+//   res.headers.set("Access-Control-Allow-Origin", "*");
+//   res.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   res.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   return res;
+// }
